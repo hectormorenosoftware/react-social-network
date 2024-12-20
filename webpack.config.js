d42ee6fb8e96da16d6bb6ce358b96ffb1150f3cc
hectormorenosoftware <hectormorenosoftware@gmail.com>
2024-12-20 13:17:57 -0600

    Added new build folder and webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   devtool: "source-map",
   entry: { index: path.resolve(__dirname, "src", "index.js") },
   output: {
-    path: path.resolve(__dirname, "build"),
+    path: path.resolve(__dirname, "dist"),
   },
   plugins: [
     new HtmlWebpackPlugin({
